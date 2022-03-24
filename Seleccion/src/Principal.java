5efe45312f1b0f8375717118bf72af94dd30e451
Humberto SC <humbertoantsc@gmail.com>
2022-03-24 10:57:06 -0600

    Agregando la practoca 10

---
+++ Seleccion/src/Principal.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+import java.util.Scanner;
+public class Principal{
+    
+    public static void main(String[] args) throws Exception {
+        
+        Seleccion sel= new Seleccion();
+        sel.desplegar();
+        sel.ordenar();
+        sel.desplegar();
+    }
+}
---
+++ Seleccion/src/Seleccion.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Seleccion {
+    private double temp[];
+    private Scanner teclado; 
+    
+    Seleccion(){
+        temp =  new double[28];
+        teclado = new Scanner(System.in);  
+        for (int i = 0; i<28; i++){
+            temp[i]=teclado.nextDouble();
+        }
+        
+    }
+    public void ordenar(){
+        System.out.println("Ordenando");
+        int i,j, min; 
+        double aux;
+        for(i=0; i<28; i=i+1){
+            min=i;
+            for(j=i+1;j<28;j=j+1){
+                if(temp[j]<temp[min])min=j;
+            }
+            aux=temp[i];
+            temp[i]=temp[min];
+            temp[min]=aux;
+        }
+    }
+    public void desplegar(){
+        System.out.println("Imprimiendo");
+        for (int i = 0; i<28; i++){
+            System.out.println(temp[i]);
+        }
+    }
+}
