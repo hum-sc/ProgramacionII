6c204d27051b85d6cb25609329eb161882e809e5
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-04-07 18:58:35 -0500

    busqueda terminada

--- BusquedaBinaria/src/BusquedaBinaria.java
+++ BusquedaBinaria/src/BusquedaBinaria.java
@@ -6,24 +6,45 @@ public class BusquedaBinaria {
             numeros[i] = (int) (Math.random()*200+1);
         }
     }
-        int busquedaBinaria(int arr[], int numeros)
-        {
-            int lo = 0, hi = arr.length - 1;
-            while (lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-    
-                if (arr[mid] == numeros)
-                    return mid;
-    
-                if (arr[mid] < numeros)
-                    lo = mid + 1;
-    
-                else
-                    hi = mid - 1;
+    public void sort(int li, int ls){
+        int piv = numeros[li];
+        int i = li;
+        int j = ls;
+        int aux;
+        while (i<j){
+            while(numeros[i]<=piv && i<j)i++;
+            while(numeros[j]>piv)j--;
+            if(i<j){
+                aux = numeros[i];
+                numeros[i]=numeros[j];
+                numeros[j]=aux;
             }
-            return -1;
         }
-        public static int buscar(int n) {
-            return 0;
+        numeros[li]=numeros[j];
+        numeros[j]=piv;
+
+        if(li < j-1){
+            sort(li, j-1);
         }
+        if(j+1<ls){
+            sort(j+1, ls);
+        }
+    }
+    public int busquedaBinaria(int n)
+    {
+        int lo = 0, hi = numeros.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (numeros[mid] == n)
+                return mid;
+
+            if (numeros[mid] < n)
+                lo = mid + 1;
+
+            else
+                hi = mid - 1;
+        }
+        return -1;
+    }
 }
--- BusquedaBinaria/src/Principal.java
+++ BusquedaBinaria/src/Principal.java
@@ -1,15 +1,17 @@
 public class Principal {
     public static void main(String[] args) throws Exception {
-        BusquedaBinaria busquedaSecuencial = new BusquedaBinaria();
+        BusquedaBinaria busquedaBinaria = new BusquedaBinaria();
         Vista vista = new Vista();
         vista.muestraMensaje("Generando el vector");
-        vista.mostrarVector(busquedaSecuencial.numeros, "Vector generado");
+        vista.mostrarVector(busquedaBinaria.numeros, "Vector generado");
+        busquedaBinaria.sort(0, 199);
+        vista.mostrarVector(busquedaBinaria.numeros, "Vector ordenado");
         int n;
         int isFinded;
         for (int i = 0; i<3; i++){
             n = vista.leeEntero("Ingresa el numero "+(i+1)+" a buscar");
         
-            isFinded = BusquedaBinaria.buscar(n);
+            isFinded = busquedaBinaria.busquedaBinaria(n);
             if (isFinded != -1){
                 vista.muestraMensaje("El numero: "+n+" esta en la posicion: "+isFinded);
             } else vista.muestraMensaje("No se encontro el numero "+n);
